4d09cf431ed0cc9c02087e8f62bfba44e8db0275
Jeesson <jason.new.channel@gmail.com>
2024-01-21 03:20:33 +0200

    Optimized date diff and opacity calculation, streamlined DOM access.

--- not-paid.js
+++ not-paid.js
@@ -1,24 +1,16 @@
-// Next time use letsdeel.com to make sure you get paid
-(function(){
-	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
-	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
-	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
-			opacity = (opacity < 0) ? 0 : opacity;
-			opacity = (opacity > 1) ? 1 : opacity;
-		if(opacity >= 0 && opacity <= 1) {
-			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
-		}
-		
-	}
-	
-})()
+(function () {
+    // Setting the expiration date and deadline date
+    const dueDate = new Date("2017-02-27");
+    const daysDeadline = 60;
+    const currentDate = new Date();
+
+    // Calculating the difference in days
+    const daysPassed = Math.floor((currentDate - dueDate) / (1000 * 60 * 60 * 24));
+
+    // Transparency calculation
+    if (daysPassed > 0) {
+        const daysRemaining = daysDeadline - daysPassed;
+        const opacity = Math.max(0, Math.min(daysRemaining / daysDeadline, 1));
+        document.body.style.opacity = opacity;
+    }
+})();
